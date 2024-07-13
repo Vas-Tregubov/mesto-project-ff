4026f08192eda093d55a178cc2e64546164fc53f
Vas Tregubov <vastregubov@gmail.com>
2024-07-13 14:42:48 +0300

    fix(modal.js): add timeout before rename fields in the form, add default properties in fields when close

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,9 +1,20 @@
 import {
   popupEditProfile,
-  popupForm,
+  popupEditProfileForm,
   editProfileFormName,
   editProfileFormDescription,
+  profileTitle,
+  profileDescription,
 } from "../index.js";
+
+const profileFormName = document.querySelector(".popup__input_type_name");
+const profileFormDescription = document.querySelector(
+  ".popup__input_type_description"
+);
+const defaultForm = {
+  profileName: (profileFormName.value = "Жак-Ив Кусто"),
+  profileJob: (profileFormDescription.value = "Исследователь океана"),
+};
 
 function openModal(obj) {
   obj.classList.add("popup_is-opened");
@@ -23,11 +34,30 @@ function closeModal(obj) {
   const closeModalCross = obj.querySelector(".popup__close");
   closeModalCross.removeEventListener("click", () => closeModal(obj));
   document.removeEventListener("keyup", () => closeModal(obj));
-  popupForm.reset();
-  editProfileFormName.value = "Жак-Ив Кусто";
-  editProfileFormDescription.value = "Исследователь океана";
-  // obj.forms.form[0]
+  const timerRename = setTimeout(() => {
+    popupEditProfileForm.reset();
+    profileFormName.value = "Жак-Ив Кусто";
+    profileFormDescription.value = "Исследователь океана";
+  }, 1000)
   // document.removeEventListener("click", () => closeModal(obj));
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  const name = profileFormName.value;
+  defaultForm.profileName = name;
+  profileTitle.textContent = name;
+  const job = profileFormDescription.value;
+  defaultForm.profileJob = job;
+  profileDescription.textContent = job;
+  // const profileFormName = evt.target.querySelector(".popup__input_type_name");
+  // const profileFormDescription = evt.target.querySelector(".popup__input_type_description");
+  // const defaultForm = {
+  //   profileName: profileFormName.value,
+  //   profileJob: profileFormDescription.value,
+  // };
+  // const { profileName: userName = 'Жак-Ив Кусто', profileJob: userJob = 'Wanker'} = defaultForm;
+  // console.log(userName, userJob);
 }
 
 // popupForm.addEventListener('submit', function(evt) {
@@ -35,4 +65,4 @@ function closeModal(obj) {
 //   popupForm.reset();
 // })
 
-export { openModal, closeModal };
+export { openModal, closeModal, handleFormSubmit };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import "./pages/index.css";
 import { initialCards } from "./scripts/cards.js";
 import { addCard, deleteCard } from "./components/card.js";
-import { openModal, closeModal } from "./components/modal.js";
+import { openModal, closeModal, handleFormSubmit } from "./components/modal.js";
 
 const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+// profile edit
+
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popupEditProfile = document.querySelector(".popup_type_edit");
+
+// add card
 
 const profileAddButton = document.querySelector(".profile__add-button");
 const popupNewCard = document.querySelector(".popup_type_new-card");
 
+// card edit
+
 const cardImage = document.querySelector(".card__image");
 const popupEditCardImage = document.querySelector(".popup_type_image");
+
+// edit profile form fields
 
 const editProfileFormName = document.querySelector(".popup__input_type_name");
 editProfileFormName.value = "Жак-Ив Кусто";
@@ -21,11 +32,14 @@ const editProfileFormDescription = document.querySelector(
   ".popup__input_type_description"
 );
 editProfileFormDescription.value = "Исследователь океана";
-const popupForm = document.querySelector(".popup__form");
+const popupEditProfileForm = document.forms["edit-profile"];
+
+// event listeners
 
 profileEditButton.addEventListener("click", () => openModal(popupEditProfile));
 profileAddButton.addEventListener("click", () => openModal(popupNewCard));
 // cardImage.addEventListener("click", () => openModal(popupEditCardImage));
+popupEditProfileForm.addEventListener("submit", handleFormSubmit);
 
 function renderInitialCards() {
   initialCards.forEach((elem) => placesList.append(addCard(elem, deleteCard)));
@@ -37,9 +51,11 @@ export {
   cardTemplate,
   placesList,
   popupEditProfile,
-  popupForm,
+  popupEditProfileForm,
   editProfileFormName,
   editProfileFormDescription,
+  profileTitle,
+  profileDescription,
 };
 
 // don't forget to call imported functions
